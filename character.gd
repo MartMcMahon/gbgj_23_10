8645998a9755ff24253fa348edd8171326221b4b
Mart <themcman@gmail.com>
2023-10-21 23:43:34 -0500

    animated guy

--- character.gd
+++ character.gd
@@ -1,12 +1,9 @@
 extends CharacterBody2D
 
+const SPEED = 150.0
+@onready var _animated_sprite = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-# var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var x_scale = 0.1
 
 func _physics_process(delta):
 	var input_direction = Vector2(
@@ -15,14 +12,22 @@ func _physics_process(delta):
 	).normalized()
 	
 	self.velocity = input_direction * SPEED
+	if (self.velocity != Vector2(0.0, 0.0)):
+		_animated_sprite.play("walk")
+		if (self.velocity.x < 0.0):
+			_animated_sprite.scale.x = -x_scale
+		else:
+			_animated_sprite.scale.x = x_scale
+	else:
+		_animated_sprite.stop()
 
 	move_and_slide()
 	light_decay(delta)
 
 
 func light_decay(delta):
-	var scale = $CandleLight.texture_scale
-	$CandleLight.texture_scale = max(scale - delta/10, 0.0)
+	var ts = $CandleLight.texture_scale
+	$CandleLight.texture_scale = max(ts - delta/100, 0.0)
 	
 	if ($CandleLight.texture_scale == 0.0):
 		# start death timer
---
+++ room.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var dist = 0.0;
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# dist += 0.1;
+	# material.set_shader_parameter("dist_to_player", dist)
+	pass
