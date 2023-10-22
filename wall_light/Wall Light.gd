3cb72978bb437465a060d771fcb38141f267ac0c
Mart <themcman@gmail.com>
2023-10-22 16:50:20 -0500

    uncessary

--- wall_light/Wall Light.gd
+++ wall_light/Wall Light.gd
@@ -6,3 +6,12 @@ func switch():
 	state = !state
 	$on.visible = state
 	$off.visible = !state
+	return state
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		body.is_in_light_area = true
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("player"):
+			body.is_in_light_area = false
