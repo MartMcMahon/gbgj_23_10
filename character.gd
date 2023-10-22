b27b0e66b43e0c9f558651a7f0f5a4ec7e8b6427
Mart <themcman@gmail.com>
2023-10-21 20:29:58 -0500

    candle glow decay

--- character.gd
+++ character.gd
@@ -9,9 +9,6 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	# if not is_on_floor():
-	# 	velocity.y += gravity * delta
 	var input_direction = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -20,3 +17,13 @@ func _physics_process(delta):
 	self.velocity = input_direction * SPEED
 
 	move_and_slide()
+	light_decay(delta)
+
+
+func light_decay(delta):
+	var scale = $CandleLight.texture_scale
+	$CandleLight.texture_scale = max(scale - delta/10, 0.0)
+	
+	if ($CandleLight.texture_scale == 0.0):
+		# start death timer
+		pass
